c66e91f1ece4ab7902d46c0f90d6d49facfa8eb4
sirekanian <sirekanian@pm.me>
2023-04-08 03:49:43 +0400

    added instrumented tests

--- README.md
+++ README.md
@@ -2,6 +2,9 @@
 
 Searchable list of bribetakers and warmongers. More info on https://acf.international.
 
+[![Build Status](https://img.shields.io/github/actions/workflow/status/sirekanian/warmongr/build.yml?label=build)](https://github.com/sirekanian/warmongr/actions/workflows/build.yml)
+[![Tests Status](https://img.shields.io/github/actions/workflow/status/sirekanian/warmongr/tests.yml?label=tests)](https://github.com/sirekanian/warmongr/actions/workflows/tests.yml)
+
 <picture>
   <source media="(prefers-color-scheme: dark)" srcset="app/src/main/play/listings/en-US/graphics/phone-screenshots/1.png">
   <img src="app/src/main/play/listings/en-US/graphics/phone-screenshots/3.png">
---
+++ app/src/androidTest/java/com/sirekanian/warmongr/AndroidTest.kt
@@ -0,0 +1,38 @@
+package com.sirekanian.warmongr
+
+import android.content.ComponentName
+import android.content.Intent
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.uiautomator.*
+import org.junit.Test
+import org.junit.runner.RunWith
+
+private const val PACKAGE = "com.sirekanian.warmongr"
+private const val ACTIVITY = "com.sirekanian.warmongr.MainActivity"
+
+@RunWith(AndroidJUnit4::class)
+class AndroidTest {
+
+    private val device = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation())
+    private val app = ApplicationProvider.getApplicationContext<App>()
+    private val intent =
+        Intent.makeMainActivity(ComponentName(PACKAGE, ACTIVITY))
+            .addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK)
+
+    @Test
+    fun putin() {
+        app.startActivity(intent)
+        wait(By.desc("Search…")).click()
+        wait(By.focused(true)).text = "путин"
+        wait(By.text("Putin Vladimir Vladimirovich"))
+    }
+
+    private fun wait(selector: BySelector): UiObject2 {
+        val condition = Until.hasObject(selector)
+        check(device.wait(condition, 60000)) { "Timeout reached" }
+        return device.findObject(selector)
+    }
+
+}
